7118a8a47ccd838c4404b447c0b2deadadaa02f8
Brian Maples <Brian.Maples@gmail.com>
2013-12-17 15:40:34 -0800

    What was originally shared on Dropbox

---
+++ PopPhased/getdata.h
@@ -0,0 +1,45 @@
+#ifndef INITIALIZATION
+#define INITIALIZATION
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+
+using namespace std;
+
+struct ProcessedInput{
+  int numSNPs;
+  int numWindows;
+  int numHaps;
+  int numAncPops;
+  int numAdm;
+  int numAnc;
+  double windowSize;
+  int numTrees;
+  int genSinceAdmixture;
+  vector<double> * windowBeginLocs;
+  vector<double> * windowEndLocs;
+  vector<int> * windowBeginIndexes;
+  vector<int> * windowEndIndexes;
+  int ** ancHapClassesPerWindow;
+  int ** ancWindows;
+  int ** admWindows;
+  string outputName;
+  bool allowParallel;
+  int bootstrapSampleSize;
+  int bootstrappingMethod;
+  int numEmIterations;
+  vector<int> * hapClasses;
+  int callAnc;    // Include anc in adm after first iteration
+  int useAnc;    // Use anc as anc after first iteration
+  int ** admWindowsPhasings;
+  int ** numHetSitesPerInd;
+  int * numAdmPhasings;
+};
+
+ProcessedInput* processInput(int argc, char ** argv, ofstream * logfile);
+
+#endif
